cae2b55105a619509e3cd690ac819030c5f318ce
Nick Sherman <ogleffeckt2@yahoo.com>
2020-04-16 13:45:00 -0400

    created a language selector component & color context to connect to language store

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from '../components/LanguageSelector';
 
 class App extends React.Component {
-	state = { language: 'english' };
-
-	onLangugeChange = (language) => {
-		this.setState({ language });
-	};
-
 	render() {
 		return (
 			<div className="ui container">
-				<div>
-					Select a language:
-					<i onClick={() => this.onLangugeChange('english')} className="flag us" />
-					<i onClick={() => this.onLangugeChange('dutch')} className="flag nl" />
-				</div>
-				<LanguageContext.Provider value={this.state.language}>
-					<UserCreate />
-				</LanguageContext.Provider>
+				<LanguageStore>
+					<LanguageSelector />
+
+					<ColorContext.Provider value="red">
+						<UserCreate />
+					</ColorContext.Provider>
+				</LanguageStore>
 			</div>
 		);
 	}
